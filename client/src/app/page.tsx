8ca139773794ef383002efe501f58599a0839661
Andre Williams <51215949+williamsa3773@users.noreply.github.com>
2024-09-13 19:28:22 -0400

    new application setup. checking env

---
+++ client/src/app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  
+  return (
+    <div>
+      <header>
+        <p>My Dictionary</p>
+        <a>Word Search</a>
+        <a>Special Words</a>
+      </header>
+
+      <main>
+        <p>Main content</p>
+      </main>
+
+      <footer>
+        <p>Footer Info</p>
+      </footer>
+    </div>
+  )
+}
